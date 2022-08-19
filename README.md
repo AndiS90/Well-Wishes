21f15db7d5702f71d90163d715858eed479dac6c
AndiS90 <87630680+AndiS90@users.noreply.github.com>
2022-08-19 05:26:42 -0500

    initial commit, copying portfolio

---
+++ README.md
@@ -0,0 +1,3 @@
+# Well Wishes Card Site
+  
+
---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import PortfolioContainer from "./components/PortfolioContainer";
+import '../src/components/pages/assets/css/Portfolio-CSS.css'
+import background from "../src/components/pages/assets/images/background-1.jpg";
+
+
+
+function App() {
+    return (
+     
+  <PortfolioContainer/>
+
+    
+    );
+  }
+
+export default App;
